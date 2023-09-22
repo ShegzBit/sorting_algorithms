83e4fbf2f4edef7448a6785cc7d65fbadb68a305
Awotubo Feranmi Gabriel <feranmiawotubo16@gmail.com>
2023-09-22 12:04:12 +0100

    fixed version marker error

--- sort.h
+++ sort.h
@@ -27,14 +27,9 @@ void print_list(const listint_t *list);
 
 /*Task functions*/
 void bubble_sort(int *array, size_t size);
-<<<<<<< HEAD
 void insertion_sort_list(listint_t **list);
 listint_t *rejoin(listint_t *list, listint_t *sorted);
 listint_t *sorted_insert(listint_t *temp, listint_t **head);
-void insertion_sort_list(listint_t **list);
-=======
 void selection_sort(int *array, size_t size);
-
->>>>>>> d50d8b0724ab00d169d9c24f03e8f67645d41e92
 
 #endif /*_SORT_H_*/

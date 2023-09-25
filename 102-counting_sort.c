f83726faae849079a69bb4e948e56d9d0498bd10
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2023-09-25 14:09:00 +0000

    counting sort revised

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,16 +1,18 @@
 #include "sort.h"
 
 /**
- * counting_sort - sorts an array of integers in ascending
+ * counting_array - sorts an array of integers in ascending
  * order using the Counting sort algorithm
  * @array: array to sort
  * @size: size of array
  */
-void counting_sort(int *array, size_t size)
+void counting_array(int *array, size_t size)
 {
-	size_t i, j = 0, max = array[0];
-	int *arr = NULL;
+	size_t i, max = array[0], j = 0;
+	int *arr;
 
+	if (array == NULL || size < 2)
+		return;
 	for (i = 1; i < size; i++)
 	{
 		if (array[j] < array[i])
@@ -31,4 +33,54 @@ void counting_sort(int *array, size_t size)
 		arr[i] += arr[i - 1];
 	print_array(arr, max + 1);
 	free(arr);
+}
+
+
+/**
+ * counting_sort - sorts an array of integers in ascending
+ * order using the Counting sort algorithm
+ * @array: array to sort
+ * @size: size of array
+ */
+void counting_sort(int *array, size_t size)
+{
+	size_t i, range, j;
+	int max, min, *count;
+
+	counting_array(array, size);
+	if (array == NULL || size < 2)
+		return;
+
+	max = array[0];
+	min = array[0];
+
+	for (i = 0; i < size; i++)
+	{
+		if (array[i] > max)
+		max = array[i];
+		if (array[i] < min)
+		min = array[i];
+	}
+	range = max - min + 1;
+	count = malloc(sizeof(int) * range);
+	if (count == NULL)
+		return;
+
+	for (i = 0; i < range; i++)
+		count[i] = 0;
+
+	for (i = 0; i < size; i++)
+		count[array[i] - min]++;
+
+	j = 0;
+	for (i = min; i <= (size_t)max; i++)
+	{
+		while (count[i - min] > 0)
+		{
+			array[j] = i;
+			count[i - min]--;
+			j++;
+		}
+	}
+	free(count);
 }

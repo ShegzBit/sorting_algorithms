23314a9e4096bd97c16935ca411373dd3f41505e
Awotubo Feranmi Gabriel <feranmiawotubo16@gmail.com>
2023-09-29 07:54:48 +0100

    pushing my merge

---
+++ 103-merge_sort.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+
+/**
+ * merge_sort - sorts an array while printing out the process
+ * using merge sort
+ * @array: the array to sort
+ * @size:size of the array
+ */
+void merge_sort(int *array, size_t size)
+{
+	(void)array;
+	(void)size;
+}
